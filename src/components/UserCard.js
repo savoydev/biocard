a89923658b8d42e9035a7bf57b3cfe4c51e48c13
savoy <chrisb1214@live.com>
2019-12-30 04:06:16 -0500

    more style updates

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,41 +4,60 @@ import COLORS from "../constants/Colors";
 import BasicCard from "./BasicCard";
 
 const StyledUserCard = styled(BasicCard)`
-  background-color: #fff;
+  background-color: ${COLORS.IVORY_BLACK};
+  min-height: 100px;
 `;
 
 const UserText = styled.div`
-  float: left;
+  text-align: center;
 `;
 
 const UserName = styled.div`
-  font-size: 2rem;
+  font-size: 1.5rem;
   font-weight: bold;
   font-family: "Heebo";
-  color: ${COLORS.PRUSSIAN_BLUE};
+  color: ${COLORS.EGGSHELL};
 `;
 
 const UserDescription = styled.div`
-  color: ${COLORS.PRUSSIAN_BLUE};
+  color: ${COLORS.EGGSHELL};
   font-size: 1rem;
 `;
 
 const UserImage = styled.div`
-  float: left;
-  background-color: #ccc;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    31deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(11, 60, 93, 1) 48%,
+    rgba(50, 140, 193, 1) 100%
+  );
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.24);
   border-radius: 100%;
-  height: 50px;
-  width: 50px;
-  margin-right: 20px;
+  height: 100px;
+  width: 100px;
+  margin: 0 auto;
+
+  div {
+    height: 90px;
+    width: 90px;
+    position: relative;
+    top: 5px;
+    left: 5px;
+    border-radius: 100%;
+    background-color: #fff;
+  }
 `;
 
 const UserCard = props => {
   return (
     <StyledUserCard>
-      {/* <UserImage /> */}
+      <UserImage>
+        <div></div>
+      </UserImage>
       <UserText>
         <UserName>{props.name}</UserName>
-        {/* <UserDescription>{props.description}</UserDescription> */}
+        <UserDescription>{props.description}</UserDescription>
       </UserText>
     </StyledUserCard>
   );
