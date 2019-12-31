93caaba4405d87a245e2446d7442d8baf0456909
savoy <chrisb1214@live.com>
2019-12-30 22:38:28 -0500

    slide up menu to choose color

--- src/components/ColorSelection.js
+++ src/components/ColorSelection.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import BasicCard from "./BasicCard";
 import COLORS from "../constants/Colors";
+import { slideInUp } from "react-animations";
+
+const slideInUpAnimation = keyframes`${slideInUp}`;
 
 const ColorSelectionStyled = styled(BasicCard)`
   text-align: center;
+  position: fixed;
+  bottom: 0;
+  margin: 0;
+  left: 0;
+  right: 0;
+  background: #fff;
+  animation: 0.5s ${slideInUpAnimation};
+  z-index: 3;
 `;
 
 const PalleteColor = styled.button`
--- src/components/LinksContainer.js
+++ src/components/LinksContainer.js
@@ -4,14 +4,40 @@ import MediaCard from "./MediaCard";
 import TextCard from "./TextCard";
 import CreateLinkCard from "./CreateLink";
 import COLORS from "../constants/Colors";
+import styled from "styled-components";
 import CARD_STYLES from "../constants/CardStyles";
 import ColorSelection from "./ColorSelection";
+
+const Button = styled.button`
+  background: ${props => props.color.DEFAULT_GRADIENT};
+  color: ${COLORS.EGGSHELL};
+  font-family: "Open Sans";
+  font-weight: bold;
+  border: none;
+  font-size: 1.25rem;
+  line-height: 2.5rem;
+  border-radius: ${props => props.cardStyle.BTN_BORDER_RADIUS};
+  margin: 0 auto;
+  display: block;
+  min-width: 100px;
+  padding: 10px 30px;
+  margin-bottom: 20px;
+  cursor: pointer;
+  box-shadow: ${props => props.cardStyle.BTN_SHADOW};
+  transition: all 0.3s;
+  :hover {
+    transform: scale(1.025);
+    transition: all 0.3s;
+    box-shadow: 0px 5px 15px rgba(130, 130, 130, 0.25);
+  }
+`;
 
 class LinksContainer extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      ShowColorSelection: false,
       Color: COLORS.IVORY_BLACK,
       CardStyle: CARD_STYLES.FLAT,
       Links: [
@@ -89,7 +115,14 @@ class LinksContainer extends React.Component {
 
   updateColor = color => {
     this.setState({
-      Color: color
+      Color: color,
+      ShowColorSelection: false
+    });
+  };
+
+  showColorSelection = () => {
+    this.setState({
+      ShowColorSelection: true
     });
   };
 
@@ -110,10 +143,20 @@ class LinksContainer extends React.Component {
           cardStyle={this.state.CardStyle}
           addLink={this.addLink}
         />
-        <ColorSelection
+        <Button
+          type="button"
+          color={this.state.Color}
           cardStyle={this.state.CardStyle}
-          updateColor={this.updateColor}
-        />
+          onClick={this.showColorSelection}
+        >
+          Change theme
+        </Button>
+        {this.state.ShowColorSelection && (
+          <ColorSelection
+            cardStyle={this.state.CardStyle}
+            updateColor={this.updateColor}
+          />
+        )}
       </React.Fragment>
     );
   }
