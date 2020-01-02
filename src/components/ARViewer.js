7025923633efcecd65f1e3e3b77ef7debb8aa928
savoy <chrisb1214@live.com>
2020-01-01 23:15:33 -0500

    end song when AR mode closed

--- src/components/ARViewer.js
+++ src/components/ARViewer.js
@@ -11,12 +11,18 @@ width: 100%;
 border: none;
 `
 
+const closeAndStopAudio = (props) => {
+    const audioAR = document.getElementById('arSong');
+    audioAR.src =""
+    props.closeAR()
+}
+
 const ARViewer = props => {
     return <SlideCard show={props.show}>
        <SlideCard.Header>Point camera at the QR code</SlideCard.Header>
     <StyledIframe src="/AR.html"></StyledIframe>
-    <Button type="button" onClick={props.closeAR}>Close</Button>
-    <audio autoPlay>
+    <Button type="button" onClick={() => {closeAndStopAudio(props)}}>Close</Button>
+    <audio id="arSong" autoPlay>
             <source src={oggAudio} type="audio/ogg" />
             <source src={mp3Audio} type="audio/mpeg" />
           Your browser does not support the audio element.
