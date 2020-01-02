7e8dbb42c7ee3b6795d583a977c3e32a3f6eecfb
savoy <chrisb1214@live.com>
2020-01-01 23:04:18 -0500

    play song in AR mode

--- src/components/ARViewer.js
+++ src/components/ARViewer.js
@@ -2,6 +2,8 @@ import React from "react"
 import SlideCard from "./Cards/SlideCard"
 import styled from "styled-components"
 import Button from "./Common/Button"
+import mp3Audio from "../audio/mixed-feelings.mp3"
+import oggAudio from "../audio/mixed-feelings.ogg"
 
 const StyledIframe = styled.iframe`
 height: 80vh;
@@ -14,6 +16,11 @@ const ARViewer = props => {
        <SlideCard.Header>Point camera at the QR code</SlideCard.Header>
     <StyledIframe src="/AR.html"></StyledIframe>
     <Button type="button" onClick={props.closeAR}>Close</Button>
+    <audio autoPlay>
+            <source src={oggAudio} type="audio/ogg" />
+            <source src={mp3Audio} type="audio/mpeg" />
+          Your browser does not support the audio element.
+          </audio> 
     </SlideCard>
 }
 
